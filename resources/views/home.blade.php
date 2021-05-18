4d166b258255c9ecfd9947d380c3dddabfcd8ea3
Dora Budić <dbudic0706@gmail.com>
2021-05-18 15:57:25 +0200

    Finish

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,15 @@
 @extends ('layouts.app')
 
 @section('main')
+<div class="card-container">
   @foreach($movies as $movie)
-  {{$movie}}
+  <div class="card">
+    <h2>Title: {{$movie->title}}</h2>
+    <h3>Original title: {{$movie->original_title}}</h3>
+    <p>Nationality: {{$movie->nationality}}</p>
+    <p>Date: {{$movie->date}}</p>
+    <p>Vote: {{$movie->vote}}</p>
+  </div>
   @endforeach
+</div>
 @endsection
